87ec4d88aac0a5e997f7c7735e41b88a87fcd194
Maksim Logunov <maksim.logunov@tele2.com>
2026-01-30 22:32:01 +0100

    Compress javascript to one file

--- web/app.js
+++ web/app.js
@@ -1,5 +1,67 @@
-import { MidiDataLogger } from "./data_logger.js";
-import { drawHistogram } from "./histogram.js";
+class MidiDataLogger {
+  constructor() {
+    this.data = [];
+    this.pressedNotes = new Map();
+    this.maxNumNotes = 100;
+    this.maxStd = 6;
+  }
+
+  setLimit(limit) {
+    this.maxNumNotes = parseInt(limit, 10);
+    this._enforceLimit();
+  }
+
+  setMaxStd(limit) {
+    this.maxStd = parseInt(limit, 10);
+  }
+
+  noteOn(note) {
+    this.pressedNotes.set(note, performance.now());
+  }
+
+  noteOff(note) {
+    const startTime = this.pressedNotes.get(note);
+    if (startTime === undefined) return;
+    const ds = performance.now() - startTime;
+    this.add(ds);
+    this.pressedNotes.delete(note);
+  }
+
+  /**
+   * Adds a value to the list.
+   * @param {number} value - The floating point value (e.g., duration).
+   */
+  add(value) {
+    this.data.push(value);
+    this._enforceLimit();
+  }
+
+  _enforceLimit() {
+    if (this.data.length > this.maxNumNotes) {
+      this.data = this.data.slice(-this.maxNumNotes);
+    }
+  }
+
+  clear() {
+    this.data = [];
+    this.pressedNotes.clear();
+  }
+
+  getAll() {
+    if (this.data.length === 0) return [];
+    if (this.maxStd === 99999) return this.data;
+
+    const n = this.data.length;
+    const mean = this.data.reduce((a, b) => a + b) / n;
+    const stdDev = Math.sqrt(this.data.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n);
+
+    if (stdDev === 0) return this.data;
+
+    const threshold = this.maxStd * stdDev + mean;
+
+    return this.data.filter(x => x <= threshold);
+  }
+}
 
 const logger = new MidiDataLogger();
 const plotElement = document.getElementById("histogram-plot");
@@ -24,6 +86,45 @@ WebMidi
   .catch(err => {
     statusBadge.innerText = err;
   });
+
+function drawHistogram(element, data) {
+  if (!data || data.length === 0) {
+    Plotly.purge(element);
+    return;
+  }
+
+  const trace = {
+    x: data,
+    type: 'histogram',
+    marker: {
+      color: '#4c74af',
+    },
+    nbinsx: 200
+  };
+
+  const latestValue = {
+    x: [data[data.length - 1]],
+    type: 'histogram',
+    marker: {
+      color: '#4caf6b',
+    },
+    nbinsx: 200
+  };
+
+  const layout = {
+    xaxis: {title: {text: 'Duration (ms)'}},
+    yaxis: {title: {text: '# Notes'}},
+    showlegend: false,
+    barmode: 'overlay'
+  };
+
+  const config = {
+    editable: false,
+    staticPlot: true,
+  };
+
+  Plotly.newPlot(element, [trace, latestValue], layout, config);
+}
 
 function updateHistogram() {
   const data = logger.getAll();
